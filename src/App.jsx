ef81457a41ec08d2f4138898edf656db92630706
“MikhelCB” <srrynow@hotmail.com>
2025-12-16 19:15:39 -0300

    feat: adicionando componentes principais e estilizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import Tasks from "./components/tasks";
 import AddTask from "./components/AddTask";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor fullstack",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Ingles",
+      description:
+        "Estudar programação para se tornar um desenvolvedor fullstack",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar espanhol",
+      description:
+        "Estudar programação para se tornar um desenvolvedor fullstack",
+      isCompleted: false,
+    },
+  ]);
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <AddTask></AddTask>
-      <Tasks></Tasks>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask></AddTask>
+        <Tasks tasks={tasks}></Tasks>
+      </div>
     </div>
   );
 }
