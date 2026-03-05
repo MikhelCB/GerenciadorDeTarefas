97f23d801b8ec4944e7bca993ec30ac2482934aa
“MikhelCB” <srrynow@hotmail.com>
2026-03-04 23:01:43 -0300

    style: alterando e adicionando icone de lixeira

--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
 function Tasks(props) {
   return (
@@ -15,6 +15,9 @@ function Tasks(props) {
           </button>
           <button className="bg-slate-400 p-2 rounded-md text-white">
             <ChevronRightIcon>Ver detalhes</ChevronRightIcon>
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <TrashIcon />
           </button>
         </li>
       ))}
